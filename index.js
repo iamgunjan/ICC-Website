ae91a54555bfd33d72c2f438824167a88c6b759e
Karan <karandhingra.contact@gmail.com>
2021-10-01 03:37:51 +0530

    Added Navbar, Footer, Hero

---
+++ index.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".mobile-nav .sidebar");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+// console.log(navLink[0].outerText);
